87ab1d04af6b1a7a4ec387cbf40ddb641987a44b
Matthew Todd <matthew.todd@gmail.com>
2009-09-27 13:12:35 +0300

    Shellwords.escape isn't available on my shared host.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,3 @@
-require 'shellwords'
-
 # I'm deploying to a Solaris machine, where setting environment variables in
 # crontabs isn't allowed.
 set :set_path_automatically, false
@@ -7,5 +5,5 @@ set :set_path_automatically, false
 # So I can't use whenever's "rake" command; I need to unroll it and inject my
 # current PATH, like so:
 every 4.hours do
-  command "cd #{Whenever.path} && /usr/bin/env PATH=#{Shellwords.escape(ENV['PATH'])} rake planet"
+  command "cd #{Whenever.path} && /usr/bin/env PATH='#{ENV['PATH']}' rake planet"
 end
