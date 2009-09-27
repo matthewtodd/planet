b16b52d9c30e6a9ff5e2119d3498eda8ccce3d54
Matthew Todd <matthew.todd@gmail.com>
2009-09-27 13:06:21 +0300

    Re-work whenever config to make Solaris' cron happy.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,11 @@
+require 'shellwords'
+
+# I'm deploying to a Solaris machine, where setting environment variables in
+# crontabs isn't allowed.
+set :set_path_automatically, false
+
+# So I can't use whenever's "rake" command; I need to unroll it and inject my
+# current PATH, like so:
 every 4.hours do
-  rake :planet
+  command "cd #{Whenever.path} && /usr/bin/env PATH=#{Shellwords.escape(ENV['PATH'])} rake planet"
 end
