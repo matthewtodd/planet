5046e081b4d19fb27d61a4987944c1964937fded
Matthew Todd <matthew.todd@gmail.com>
2009-09-27 12:25:08 +0300

    Symlink to the shared directory for venus' cache.

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,10 +5,27 @@ set :deploy_via,  :remote_cache
 set :scm,         :git
 set :use_sudo,    false
 
+# Capistrano defaults to creating system, log, and pids under the shared_path,
+# but I need cache (for venus to cache its downloaded feeds) and log.
+set :shared_children, %w(cache log)
+
 server 'woodward.joyent.us', :web, :app, :db, :primary => true, :user => 'matthew'
 
 namespace :deploy do
   task(:start)   { }
   task(:stop)    { }
   task(:restart) { }
+
+  # I've overridden finalize_update rather than hook into it because I need
+  # more directories created, and one command is significantly faster than 2
+  # on my slow connection.
+  desc '[internal] Touches up the released code.'
+  task :finalize_update, :except => { :no_release => true } do
+    run <<-CMD
+      rm -rf #{latest_release}/log #{latest_release}/tmp/cache &&
+      mkdir -p #{latest_release}/tmp &&
+      ln -s #{shared_path}/cache #{latest_release}/tmp/cache &&
+      ln -s #{shared_path}/log #{latest_release}/log
+    CMD
+  end
 end
