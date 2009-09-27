eaac5082264fb14a3a33a629d579a3abcfc0f8b8
Matthew Todd <matthew.todd@gmail.com>
2009-09-27 17:48:01 +0300

    Turn on logging for cron.

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,5 +5,5 @@ set :set_path_automatically, false
 # So I can't use whenever's "rake" command; I need to unroll it and inject my
 # current PATH, like so:
 every 4.hours do
-  command "cd #{Whenever.path} && /usr/bin/env PATH='#{ENV['PATH']}' rake planet"
+  command "cd #{Whenever.path} && /usr/bin/env PATH='#{ENV['PATH']}' rake planet", :cron_log => 'log/venus.log'
 end
